8abd884a8898b81aea7b716994bd2c656b95c6dd
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-09-18 10:15:59 +0100

    remove conflite in api.php

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-<<<<<<< HEAD
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-=======
-
 require base_path('routes/api/v1.php');
->>>>>>> e2e42069c7ebd1c077ca83cefa468716701e5357
